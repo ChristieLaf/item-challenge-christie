fc797fd780cb782f896495332cabb1973251f332
Christie Laferriere <christie.laferriere@gmail.com>
2026-04-19 00:23:43 -0400

    restore example.ts with note on handler directory structure

---
+++ src/handlers/example.ts
@@ -0,0 +1,7 @@
+// Handlers are separated into their own directories under src/handlers/
+
+// Each handler has its own index.ts file:
+    // - src/handlers/createItem/index.ts
+    // - src/handlers/getItem/index.ts
+    // - src/handlers/updateItem/index.ts
+    // - src/handlers/listItems/index.ts
