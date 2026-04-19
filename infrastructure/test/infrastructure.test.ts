866da83261aef74d540b7e7f0ce6c1c2943cfcee
Christie Laferriere <christie.laferriere@gmail.com>
2026-04-19 00:28:36 -0400

    add CDK infrastructure stack tests
    - Test DynamoDB table creation with correct partition key and billing mode
    - Test all five Lambda functions are created with correct runtime and handlers
    - Test API Gateway is created with correct name
    - Test authorizer is configured with x-api-key identity source and 0 TTL
    - Test DynamoDB table deletion policy is set to Delete for non-prod
    - Add jest to types array in infrastructure tsconfig.json to resolve TypeScript errors

--- infrastructure/test/infrastructure.test.ts
+++ infrastructure/test/infrastructure.test.ts
@@ -1,17 +1,71 @@
-// import * as cdk from 'aws-cdk-lib/core';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Infrastructure from '../lib/infrastructure-stack';
+import * as cdk from 'aws-cdk-lib/core';
+import { Template } from 'aws-cdk-lib/assertions';
+import { InfrastructureStack } from '../lib/infrastructure-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/infrastructure-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Infrastructure.InfrastructureStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+describe('InfrastructureStack', () => {
+  const app = new cdk.App();
+  const stack = new InfrastructureStack(app, 'TestStack', {
+    stage: 'test',
+  });
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  test('DynamoDB table is created with correct partition key', () => {
+    template.hasResourceProperties('AWS::DynamoDB::Table', {
+      TableName: 'exam-items-test',
+      BillingMode: 'PAY_PER_REQUEST',
+      KeySchema: [
+        { AttributeName: 'id', KeyType: 'HASH' },
+      ],
+    });
+  });
+
+  test('All five Lambda functions are created with correct runtime', () => {
+    template.resourceCountIs('AWS::Lambda::Function', 5);
+    template.hasResourceProperties('AWS::Lambda::Function', {
+      FunctionName: 'exam-items-test-createExamItem',
+      Runtime: 'nodejs22.x',
+      Handler: 'index.createItemHandler',
+    });
+    template.hasResourceProperties('AWS::Lambda::Function', {
+      FunctionName: 'exam-items-test-getExamItem',
+      Runtime: 'nodejs22.x',
+      Handler: 'index.getItemHandler',
+    });
+    template.hasResourceProperties('AWS::Lambda::Function', {
+      FunctionName: 'exam-items-test-updateExamItem',
+      Runtime: 'nodejs22.x',
+      Handler: 'index.updateItemHandler',
+    });
+    template.hasResourceProperties('AWS::Lambda::Function', {
+      FunctionName: 'exam-items-test-listExamItems',
+      Runtime: 'nodejs22.x',
+      Handler: 'index.listItemsHandler',
+    });
+    template.hasResourceProperties('AWS::Lambda::Function', {
+      FunctionName: 'exam-items-test-authorizer',
+      Runtime: 'nodejs22.x',
+      Handler: 'index.handler',
+    });
+  });
+
+  test('API Gateway is created with correct name', () => {
+    template.hasResourceProperties('AWS::ApiGateway::RestApi', {
+      Name: 'Exam API test',
+    });
+  });
+
+  test('API Gateway authorizer is created with correct identity source', () => {
+    template.hasResourceProperties('AWS::ApiGateway::Authorizer', {
+      Name: 'exam-items-test-authorizer',
+      IdentitySource: 'method.request.header.x-api-key',
+      Type: 'REQUEST',
+      AuthorizerResultTtlInSeconds: 0,
+    });
+  });
+
+  test('DynamoDB table is destroyed on stack deletion in non-prod', () => {
+    template.hasResource('AWS::DynamoDB::Table', {
+      DeletionPolicy: 'Delete',
+    });
+  });
 });
